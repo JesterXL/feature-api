5d790fe0565b4882ee0a72676466d5564e0fe5f3
Jesse Warden <jesterxl@jessewarden.com>
2018-09-02 18:13:38 -0400

    refactored to functional

--- src/app.js
+++ src/app.js
@@ -1,42 +1,80 @@
 const express = require('express')
-const app = express()
 const { Pool } = require('pg')
-const pool = new Pool()
+const Result = require('folktale/result')
+const { Ok } = Result
+const Validation = require('folktale/validation')
+const { Success, Failure } = Validation
+const { get, partial, memoize, curry } = require('lodash/fp')
 
-app.get('/users/list', (req, res) => {
-    return pool.query('SELECT username,date FROM users;')
-    .then(result => result.rows)
-    .then(users => {
-        res.json({result: true, data: users})
-    })
-    .catch(err => {
-        res.json({result: false, error: err.message})
-    })
-})
+const getPool = () => new Pool()
+const getPoolCached = memoize(getPool)
+
+const validRows = o =>
+    Array.isArray(o)
+    ? Success(o)
+    : Failure([`Invalid Rows: Postgres didn't return an Array of rows, instead returned: ${o}`])
+
+const getUsers = pool =>
+    pool.query('SELECT username,date FROM users;')
+    .then(get('rows'))
+    .then(validRows)
+    .then(validation =>
+        validation.matchWith({
+           Failure: ({value}) => Result.Error(value),
+           Success: ({value}) => Ok(value)
+        }))
+    .catch(error => Promise.resolve(
+        Result.Error(`Postgres Error: ${get('message', error)}`)
+    ))
+const getUsersPartial = partial(getUsers, [getPoolCached()])
+
+const sendError = res => error =>
+    res.json({ result: false, error })
+
+const sendOk = res => data =>
+    res.json({ result: true, data })
+
+const getUsersRoute = curry( (getUsersPartial, req, res) =>
+    getUsersPartial()
+    .then(result =>
+        result.matchWith({
+            Error: ({value}) => sendError(res)(value),
+            Ok: ({value}) => sendOk(res)(value)
+        })
+    ))
+
+const getUsersRoutePartial = getUsersRoute(getUsersPartial)
+
+const addUsersListRoute = app => route =>
+    Ok(
+        app.get('/users/list', route)
+    )
+
+const listen = port => app =>
+    Result.try(
+        () => app.listen(port)
+    )
 
 let server
+const startServer = getUsersRoutePartial => port => app =>
+    addUsersListRoute(app)(getUsersRoutePartial)
+    .chain(_ => listen(port)(app))
+    .chain(newServer => {
+        server = newServer
+        return Result.Ok(server)
+    })
+    .matchWith({
+        Error: ({value}) => Promise.reject(value),
+        Ok: ({value}) => Promise.resolve(server)
+    })
+const startServerPartial = () => startServer(getUsersRoutePartial)(3000)(express())
 
-const startServer = () => {
-    return new Promise( resolve => {
-        server = app.listen(3000, () => {
-            resolve(server)
-        })
-    })
-}
-const stopServer = () =>
+const stopServer = pool =>
     pool.end()
-    .then(() => {
-        return new Promise((success, failure) => {
-            server.close( err => {
-                if(err) {
-                    return failure(err)
-                }
-                success()
-            })
-        })
-    })
+    .then( _ => server.close())
+const stopServerPartial = partial(stopServer, [getPoolCached()])
 
 module.exports = {
-    startServer,
-    stopServer
+    startServer: startServerPartial,
+    stopServer: stopServerPartial
 }
